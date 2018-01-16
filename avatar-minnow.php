0e76e3e9a0e24421292daf4d9a546a8e22f519e5
Fabi Yamada <33674731+fabiyamada@users.noreply.github.com>
2018-01-16 12:21:58 -0700

    Add files via upload

---
+++ avatar-minnow.php
@@ -0,0 +1,54 @@
+<?php
+
+build_monster($_REQUEST['seed'],$_REQUEST['size']);
+
+function build_monster($seed='',$size=''){
+    // init random seed
+    if($seed) srand( hexdec(substr(md5($seed),0,6)) );
+
+    // throw the dice for body parts
+    $parts = array(
+        'body' => rand(1,2),
+        'hands' => rand(1,2),
+        'eyes' => rand(1,2),
+        'mouth' => rand(1,2),
+        'accessorie' => rand(1,2),
+        'tentacles' => rand(1,2)
+    );
+
+    // create backgound
+    $monster = @imagecreatetruecolor(256, 256)
+        or die("GD image create failed");
+    $white   = imagecolorallocate($monster, 255, 255, 255);
+    imagefill($monster,0,0,$white);
+
+    // add parts
+    foreach($parts as $part => $num){
+        $file = dirname(__FILE__).'/img-minnow/'.$part.'_'.$num.'.png';
+
+        $im = @imagecreatefrompng($file);
+        if(!$im) die('Failed to load '.$file);
+        imageSaveAlpha($im, true);
+        imagecopy($monster,$im,0,0,0,0,256,256);
+        imagedestroy($im);
+
+    }
+
+    // restore random seed
+    if($seed) srand();
+
+    // resize if needed, then output
+    if($size && $size < 400){
+        $out = @imagecreatetruecolor($size,$size)
+            or die("GD image create failed");
+        imagecopyresampled($out,$monster,0,0,0,0,$size,$size,256,256);
+        header ("Content-type: image/png");
+        imagepng($out);
+        imagedestroy($out);
+        imagedestroy($monster);
+    }else{
+        header ("Content-type: image/png");
+        imagepng($monster);
+        imagedestroy($monster);
+    }
+}
